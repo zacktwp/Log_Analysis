cc454eb6adf30b5dca382fd1d0e396cfb4aea235
zacktwp <zacktwp@gmail.com>
2019-05-10 12:05:26 -0700

    adding all files

---
+++ Logs_Analysis_project.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import psycopg2
+
+# queries
+query1 = """SELECT log.path, count(log.path) AS views
+            FROM log
+            GROUP BY log.path
+            ORDER BY views DESC
+            LIMIT 3;"""
+
+query2 = """SELECT authors.name, count(*) as views
+            FROM articles
+            inner JOIN authors on articles.author = authors.id
+            inner JOIN log on log.path LIKE concat('%', articles.slug, '%')
+            GROUP BY authors.name
+            ORDER BY views DESC;"""
+
+query3 = """SELECT *
+            FROM (SELECT date(time),round(100.0*sum(CASE log.status
+            WHEN '200 OK'  THEN 0 ELSE 1 END)/count(log.status),2) AS error
+            FROM log
+            GROUP BY date(time)
+            ORDER BY error DESC) AS subquery
+            WHERE error > 1;"""
+
+# connect to the database
+db = psycopg2.connect(database='news')
+cursor = db.cursor()
+# Execute queries
+cursor.execute(query1)
+# Return result of the query
+result1 = cursor.fetchall()
+print(result1)
+
+cursor.execute(query2)
+# Return result of the query
+result2 = cursor.fetchall()
+print(result2)
+
+cursor.execute(query3)
+# Return result of the query
+result3 = cursor.fetchall()
+print(result3)
+
+db.close()
---
+++ README.md
@@ -0,0 +1,20 @@
+## Log Analysis project
+
+In this project, you are expected to write PostgreSQL queries in a python file to answer the three questions below:
+
+1. What are the most popular three articles of all time?
+2. Who are the most popular article authors of all time?
+3. On which days did more than 1% of requests lead to errors?
+
+## Dependencies:
+python 3.6
+PostgreSQL 9.5.16
+Vagrant
+VirtualBox
+newsdata.sql
+
+## How to run:
+1. Install Dependencies
+2. create virtual machine in directory of choice
+3. Navigate to the /vagrant directory where you will find Logs_Analysis_project.py
+4. type: python Logs_Analysis_project.py into the terminal and you will see your results
